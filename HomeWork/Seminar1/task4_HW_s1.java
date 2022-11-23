62cbfcae7abbca1629f84ba0c3ea55b85c6c727d
ZLoDoc <zlodoc@rambler.ru>
2022-11-23 09:24:09 +0300

    task4 started

---
+++ HomeWork/Seminar1/task4_HW_s1.java
@@ -0,0 +1,20 @@
+// Задание 4.
+// Задано уравнение вида q + w = e, где q, w, e >= 0. 
+// Некоторые цифры могут быть заменены знаком вопроса, например 2? + ?5 = 69. 
+// Требуется восстановить выражение до верного равенства. 
+// Предложить хотя бы одно решение или сообщить, что его нет.
+package HomeWork.Seminar1;
+import java.util.Scanner;
+
+public class task4_HW_s1 {
+    public static void main(String[] args) {
+        Scanner input =  new Scanner(System.in);
+        System.out.printf("Введите число n: ");
+        int n = input.nextInt();
+        System.out.println(n);
+        input.close();
+        
+
+
+    }
+}
