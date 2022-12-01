43acccab7e6d38668cf85dac377bd914d3ed2727
ZLoDoc <zlodoc@rambler.ru>
2022-12-01 22:38:21 +0300

    HomeWork4 for Seminar3 completed

--- HomeWork/Seminar3/task4_HW_s3.java
+++ HomeWork/Seminar3/task4_HW_s3.java
@@ -7,7 +7,6 @@ package HomeWork.Seminar3;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class task4_HW_s3 {
@@ -25,43 +24,31 @@ public class task4_HW_s3 {
         while(true){
             for( int i=0;i<shelf.size();i++){
                 System.out.println(shelf.get(i).get(0));
-
             }
-       Scanner input =  new Scanner(System.in);
-       System.out.printf("Выберите раздел: ");
-       String chapter = input.nextLine().toLowerCase();
-    
-       boolean present;
-       int currentSection=0;
-       
-       for(int i = 0;i<shelf.size();i++){
-        System.out.println(shelf.get(i).get(0));
-
-            if (shelf.get(i).get(0).equals(chapter)){// Проверяем есть ли введенный раздел n
-                System.out.println(shelf.get(i).get(0));
-                present = true;
-                currentSection = i;
-                System.out.println("lalalal");
-                break;                
-            } 
-                           
-           else if(i == shelf.size()-1){
-                    shelf.add(new ArrayList<String>(Arrays.asList(chapter)));
-                    System.out.println("dfdfdfdfdfdfdf");
-                    present = true;
-                    currentSection = shelf.size();
+            Scanner input =  new Scanner(System.in);
+            System.out.println("Choise chapter:" );
+            String chapter = input.nextLine().toLowerCase();    
+            int currentSection=0;       
+            for(int i = 0;i<shelf.size();i++){
+                if (shelf.get(i).get(0).equals(chapter)){                                   
+                    currentSection = i;                
+                    break;                
+                }                           
+                else if(i == shelf.size()-1){
+                    shelf.add(new ArrayList<String>(Arrays.asList(chapter)));                                       
+                    currentSection = shelf.size()-1;
                     break;
-           }
-        }               
-                System.out.println("Введите название книги : ");
-                String bookName = input.nextLine().toLowerCase();
-
-                if(!shelf.get(currentSection-1).contains(bookName)){      // Проверяем есть ли такая книга
-                    shelf.get(currentSection).add(bookName);
-                }             
-    
-    System.out.println(shelf.toString());
-    System.out.println(currentSection);
+                }
+            }            
+            System.out.println("Enter the name of the book : ");
+            String bookName = input.nextLine().toLowerCase();
+            if(!shelf.get(currentSection).contains(bookName)){                 
+                shelf.get(currentSection).add(bookName);
+            }                         
+            System.out.println(shelf.toString());
+            System.out.println(currentSection);
+            System.out.println(shelf.get(currentSection).size());
+            System.out.println(shelf.size());
         }
 }
 }
