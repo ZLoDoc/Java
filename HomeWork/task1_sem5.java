ec393857daacde244743cf847721e835e4bcd6bf
ZLoDoc <zlodoc@rambler.ru>
2022-12-06 23:46:53 +0300

    HW5

---
+++ HomeWork/task1_sem5.java
@@ -0,0 +1,20 @@
+// Задача1
+// Реализуйте структуру телефонной книги с помощью HashMap, 
+// учитывая, что 1 человек может иметь несколько телефонов.
+package HomeWork;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class task1_sem5 {
+    public static void main(String[] args) {
+        Map<Integer, String> db = new HashMap<>();
+        // ArrayList<Integer> list = new ArrayList<>();
+        LinkedList<String> addList = new LinkedList<>();
+        db.put(1, );
+        
+    }
+    
+}
