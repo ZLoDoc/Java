b4bd6d6e2cbb466f01be7733a781c6205a960299
ZloDoc <zlodoc@rambler.ru>
2022-12-14 11:25:34 +0300

    changed value class into HashMap

--- HomeWork/Seminar6/main.java
+++ HomeWork/Seminar6/main.java
@@ -10,25 +10,23 @@ public class main {
     public static void main(String[] args) {      
         
 
-        HashMap<Integer,Object> notes = new HashMap<>();
+        HashMap<Integer,note> notes = new HashMap<>();
         for (int i = 1; i<=10;i++ ){
             note first = new note(i);
             first.info();
-            notes.put(i, first);
-            
-           
+            notes.put(i, first);           
         }
+        
+        
         // System.out.println(notes.get(1));
         // Object temp = (notes.get(1));
         
         
         for (Map.Entry key: notes.entrySet()) {
-            // System.out.println(key.getValue());
-            // key.getValue().equals(null);
-                // System.out.println(key.getValue().getClass());
-                Class[] interfaces = key.getValue().getClass().getInterfaces();                            
-
+            System.out.println(key.getValue().getClass().toGenericString());
+            
         }   
+       
            
             
         
